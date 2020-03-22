ebea2777861b1c35afcaa0f0f7f6b7e16cc5a823
Polina Barabash <polina_11.92@inbox.ru>
2020-03-22 15:31:52 +0300

    implement carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,17 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {  
+  const k = 0.693/HALF_LIFE_PERIOD;
+  let numberSampleActivity = parseFloat(sampleActivity);
+
+  if (typeof sampleActivity !== 'string' 
+      || (numberSampleActivity <= 0) 
+      || (numberSampleActivity > MODERN_ACTIVITY)
+      || !sampleActivity
+      || isNaN(numberSampleActivity)) {
+    return false;
+  }  
+  let age = Math.ceil(Math.log(MODERN_ACTIVITY/numberSampleActivity)/k);
+  return age;
 };
